472e031b9c61f50afc6bba57ae6fb9ce3646629e
yona <04l.yona@gmail.com>
2018-11-19 12:17:24 -0500

    Hook up apollo client and import songlist component

--- client/index.js
+++ client/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client'; // interacts with graphql server on backend (makes request)
+import { ApolloProvider } from 'react-apollo';
+import SongList from './components/SongList';
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <SongList />
+    </ApolloProvider>
+  );
 };
 
 ReactDOM.render(
