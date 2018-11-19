6b05fee2baad96035168b1c8f32ac364da87053f
yona <04l.yona@gmail.com>
2018-11-19 14:23:45 -0500

    Component to create song

---
+++ client/components/SongCreate.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class SongCreate extends Component {
+  render() {
+    return (
+      <div>
+        hola u create song title here :D 
+        <form>
+        </form>
+      </div>
+    );
+  }
+}
+
+// write mutation to create song title
+
+export default SongCreate;
