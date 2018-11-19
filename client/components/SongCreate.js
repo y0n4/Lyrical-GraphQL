18abda31519634f92db0e026b80283469bacec5c
yona <04l.yona@gmail.com>
2018-11-19 17:29:46 -0500

    Component includes form to submit song title and save the title to database using graphql

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class SongCreate extends Component {
+  constructor(props) {
+     super(props);
+     this.state = {
+      title: '',
+     }
+  }
+
+  // makes graphql to save the song title to database when submitted
+  onSubmit(event) {
+    event.preventDefault(); // prevents form to submit itself
+    
+    this.props.mutate({
+      variables: {
+        title: this.state.title
+      }
+    })
+  }
+
   render() {
     return (
       <div>
-        hola u create song title here :D 
-        <form>
+        <h1>Create new song</h1>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <label>Song Title:</label>
+          <input onChange={event => this.setState({title: event.target.value})} value={this.state.title}/>
         </form>
       </div>
     );
@@ -13,5 +35,13 @@ class SongCreate extends Component {
 }
 
 // write mutation to create song title
+const mutation = gql`
+  mutation AddSong($title: String){
+    addSong(title: $title) {
+      id
+      title
+    }
+  }
+`;
 
-export default SongCreate;
+export default graphql(mutation)(SongCreate);
