76f6f1782a1578f0ce6a279b11ed425063f5f5dc
yona <04l.yona@gmail.com>
2018-12-14 12:46:56 -0500

    Graphql mutation syntax to delete a specific song w given id

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -6,7 +6,7 @@ import { graphql } from 'react-apollo';
 import query from '../queries/fetchSongs'
 
 class SongList extends Component {
-  renderSongs() {
+  renderSongs() { // renders all songs into a list form
     return this.props.data.songs.map(song => {
       return(
         <li key={song.id} className="collection-item">
@@ -34,4 +34,15 @@ class SongList extends Component {
   }
 }
 
-export default graphql(query)(SongList); // calls query first then renders SongList component
+const mutation = gql`
+  mutation DeleteSong($id:ID) {
+  deleteSong(id: $id) {
+    id
+    title
+  }
+}
+`;
+
+export default graphql(mutation) (
+  graphql(query)(SongList)
+) // calls query first then renders SongList component
