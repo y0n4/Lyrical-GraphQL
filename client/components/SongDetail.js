d437d0aa58b64b1a854a20b7f9d9b8822c069781
yona <04l.yona@gmail.com>
2018-12-17 13:41:12 -0500

    Connect song detail query to component

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import fetchSongDetail from '../queries/fetchSongDetail';
 
 class SongDetail extends Component {
   render() {
-    return (
+    console.log(this.props);
+    return ( 
       <div>
         <h3>Song Detail</h3>
       </div>
@@ -10,4 +13,9 @@ class SongDetail extends Component {
   }
 }
 
-export default SongDetail;
+export default graphql(fetchSongDetail, {
+  options: (props) => {
+    return { variables: { id: props.params.id } } }
+})(SongDetail);
+
+// export default SongDetail;
