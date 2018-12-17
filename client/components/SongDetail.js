ca626a6a1c554012d620bbaf0d8d5b51dc1eb033
yona <04l.yona@gmail.com>
2018-12-17 13:52:27 -0500

    Add back button which goes to song list page

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 import fetchSongDetail from '../queries/fetchSongDetail';
 
 class SongDetail extends Component {
   render() {
-    console.log(this.props);
-    return ( 
+    const { song } = this.props.data;
+    if (!song) { return <div>Loading...</div> } // while retrieving info
+    
+    return (
       <div>
-        <h3>Song Detail</h3>
+        <Link
+          to="/"
+        >
+          <i className="material-icons">arrow_back_ios</i>
+        </Link>
+        <h3>{song.title}</h3>
       </div>
     )
   }
@@ -17,5 +25,3 @@ export default graphql(fetchSongDetail, {
   options: (props) => {
     return { variables: { id: props.params.id } } }
 })(SongDetail);
-
-// export default SongDetail;
